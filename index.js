1e3f870016bd1600896ebe422d7eba8c752cee68
Pratyush Jena <pratyushjena8@gmail.com>
2022-06-12 12:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: 'd9835cc5',
         s: searchTerm
@@ -45,7 +45,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: 'd9835cc5',
       i: movie.imdbID
